65261e6c3c1de03a71d5f66e2a199e2de12dee6d
kimbeomjin95 <kimbeomjin95@github.com>
2021-11-29 21:46:44 +0900

    97. 재사용 가능한 "카드" 컴포넌트 추가하기

--- src/component/Users/AddUser.js
+++ src/component/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import classes from './AddUser.module.css'
+import Card from "../UI/Card";
 
 const AddUser = (props) => {
 	const addUserHandler = (e) => {
@@ -6,13 +8,15 @@ const AddUser = (props) => {
 	};
 
 	return (
-		<form onSubmit={addUserHandler}>
-			<label htmlFor="username">Username</label>
-			<input id='username' type="text" />
-			<label htmlFor="age">Age (Years)</label>
-			<input id='age' type="number" />
-			<button type="submit">Add User</button>
-		</form>
+		<Card className={classes.input}>
+			<form onSubmit={addUserHandler}>
+				<label htmlFor="username">Username</label>
+				<input id='username' type="text" />
+				<label htmlFor="age">Age (Years)</label>
+				<input id='age' type="number" />
+				<button type="submit">Add User</button>
+			</form>
+		</Card>
 	);
 };
 
