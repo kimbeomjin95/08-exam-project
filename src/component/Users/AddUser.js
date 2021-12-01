71b4e8f6ac0715497f3b3db8007a7c4962d14dbc
kimbeomjin95 <kimbeomjin95@github.com>
2021-12-01 21:25:44 +0900

    98. 재사용 가능한 "Button" 컴포넌트 추가하기

--- src/component/Users/AddUser.js
+++ src/component/Users/AddUser.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import classes from './AddUser.module.css'
-import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
 
-const AddUser = (props) => {
-	const addUserHandler = (e) => {
-		e.preventDefault()
-	};
+const AddUser = props => {
+  const addUserHandler = e => {
+    e.preventDefault();
+  };
 
-	return (
-		<Card className={classes.input}>
-			<form onSubmit={addUserHandler}>
-				<label htmlFor="username">Username</label>
-				<input id='username' type="text" />
-				<label htmlFor="age">Age (Years)</label>
-				<input id='age' type="number" />
-				<button type="submit">Add User</button>
-			</form>
-		</Card>
-	);
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
 };
 
 export default AddUser;
