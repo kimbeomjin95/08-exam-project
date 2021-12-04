073c032785dcbd9815c0a787b853c0c37996201e
kimbeomjin95 <kimbeomjin95@github.com>
2021-12-04 14:00:06 +0900

    104. 오류 State 관리하기

--- src/component/Users/AddUser.js
+++ src/component/Users/AddUser.js
@@ -7,17 +7,26 @@ import ErrorModal from '../UI/ErrorModal';
 const AddUser = props => {
   const [userName, setUserName] = useState('');
   const [age, setAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = e => {
     e.preventDefault();
 
     // 이름, 나이 벨리데이션 체크
     if (userName.trim().length === 0 || age.trim().length === 0) {
+      setError({
+        title: '입력값을 다시 확인해주세요.',
+        message: '이름과 나이를 입력해주세요.',
+      });
       return; // 함수 종료
     }
 
     // string -> int 변환
     if (+age < 1) {
+      setError({
+        title: '나이를 다시 입력하세요',
+        message: '나이는 0보다 작을 수 없습니다.',
+      });
       return;
     }
 
@@ -35,9 +44,20 @@ const AddUser = props => {
     setAge(e.target.value);
   };
 
+  // 조건부 모달 렌더링 state 비우기
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <Fragment>
-      <ErrorModal title="제목" message="내용" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
