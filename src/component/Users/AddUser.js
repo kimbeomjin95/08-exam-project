688f95dbf72821b6bea06991154e9f1e71962e46
kimbeomjin95 <kimbeomjin95@github.com>
2021-12-04 15:42:28 +0900

    112. "ref"로 작업하기

--- src/component/Users/AddUser.js
+++ src/component/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,15 +6,17 @@ import ErrorModal from '../UI/ErrorModal';
 import Wrapper from '../Helper/Wrapper';
 
 const AddUser = props => {
-  const [userName, setUserName] = useState('');
-  const [age, setAge] = useState('');
   const [error, setError] = useState();
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const addUserHandler = e => {
     e.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
 
     // 이름, 나이 벨리데이션 체크
-    if (userName.trim().length === 0 || age.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: '입력값을 다시 확인해주세요.',
         message: '이름과 나이를 입력해주세요.',
@@ -23,7 +25,7 @@ const AddUser = props => {
     }
 
     // string -> int 변환
-    if (+age < 1) {
+    if (+enteredAge < 1) {
       setError({
         title: '나이를 다시 입력하세요',
         message: '나이는 0보다 작을 수 없습니다.',
@@ -31,18 +33,9 @@ const AddUser = props => {
       return;
     }
 
-    props.onAddUser(userName, age);
-    console.log(userName, age);
-    setUserName('');
-    setAge('');
-  };
-
-  const userNameChangeHandler = e => {
-    setUserName(e.target.value);
-  };
-
-  const ageChangeHandler = e => {
-    setAge(e.target.value);
+    props.onAddUser(enteredName, enteredAge);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
   // 조건부 모달 렌더링 state 비우기
@@ -62,19 +55,9 @@ const AddUser = props => {
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={userName}
-            onChange={userNameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            value={age}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
